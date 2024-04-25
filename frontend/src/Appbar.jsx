a80d13a2b52f1fb9a03d926a6db44615cc67a541
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-04-25 05:53:42 +0000

    adding logout

--- frontend/src/Appbar.jsx
+++ frontend/src/Appbar.jsx
@@ -1,56 +1,90 @@
 import { Button, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
-function Appbar(){
+function Appbar() {
 
-   const navigate = useNavigate();
+    const navigate = useNavigate();
+    const found = false
+    const [username, setUsername] =  useState("")
     const url = `https://fantastic-happiness-jjrgp4974647f5rr5-8000.app.github.dev/admin/me`;
     useEffect(() => {
         const token = localStorage.getItem("token");
-        if (token) {
+        
             fetch(`${url}`, {
                 method: 'GET',
                 headers: {
-                    "authorization": `Bearer ${token}`
+                    "authorization": `Bearer ${localStorage.getItem("token")}`
                 }
             }).then((res) => {
                 res.json().then((data) => {
+                    setUsername(data.username)
+                 //   found = true
                     console.log("DATA : " + data);
                 });
             }).catch((err) => {
                 console.error(err);
             });
-        } else {
-            console.log("No token found in localStorage");
-        }
+        
     }, []);
-    
-    return(
-        <>
+
+
+    if (username) {
+       return (
+        
+         <>
+         fjdsfjlksjfljdsfj
             <div style={{
-                display:"flex",
-                justifyContent:"space-between"
+                display: "flex",
+                justifyContent: "space-between"
             }}>
 
-                <div style={{padding:20}}>
+                <div style={{ padding: 20 }}>
                     <Typography fontSize={20}>Coursera</Typography>
                 </div>
 
-                <div style={{display:"flex", padding: 20}}>
-                    <div style={{marginRight:10}}>
-                        <Button  variant={"outlined"}
-                                 onClick={() => {
-                                     navigate('/signup');
-                                 }}
+                <div style={{ display: "flex", padding: 20 }}>
+                    <div style={{ marginRight: 10 }}>
+                        <Button variant={"outlined"}
+                            onClick={() => {
+                                localStorage.setItem("token",null)
+                            }}
+                        >Logout</Button>
+                    </div>
+
+
+                </div>
+            </div>
+        
+        </>
+       )
+    }
+
+    return (
+        <>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-between"
+            }}>
+
+                <div style={{ padding: 20 }}>
+                    <Typography fontSize={20}>Coursera</Typography>
+                </div>
+
+                <div style={{ display: "flex", padding: 20 }}>
+                    <div style={{ marginRight: 10 }}>
+                        <Button variant={"outlined"}
+                            onClick={() => {
+                                navigate('/signup');
+                            }}
                         >Signup</Button>
                     </div>
 
                     <div>
                         <Button variant={"outlined"}
-                                onClick={() => {
-                                    navigate('/login');
-                                }}
+                            onClick={() => {
+                                navigate('/login');
+                            }}
                         >Signin</Button>
                     </div>
 
